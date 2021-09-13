40709602fcfcec4fa6102001212309fea53d1387
farefray <30721331+farefray@users.noreply.github.com>
2021-09-13 15:19:23 +0300

    addded preview images and correcting colemak for my own needs

--- keymap.c
+++ keymap.c
@@ -13,11 +13,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   ;  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | BcSp |   A  |   R  |   S  |   T  |   G  |-------.    ,-------|   M  |   N  |   E  |   I  |   O  |  `   |
+ * | [    |   A  |   R  |   S  |   T  |   G  |-------.    ,-------|   M  |   N  |   E  |   I  |   O  |  `   |
  * |------+------+------+------+------+------|   ,   |    |    .  |------+------+------+------+------+------|
- * |   [  |   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   '  |   \  |   /  |  ]   |
+ * | LCtl |   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   '  |   \  |   /  |  ]   |
  * `-----------------------------------------/ LShift/     \ UTIL \-----------------------------------------'
- *                   | LCtl | LAlt |  Meh | /[Space]/       \[Entr]\  | PAGE | RAlt | LGUI |
+ *                   | BcSp | LAlt |  Meh | /[Space]/       \[Entr]\  | PAGE | RAlt | LGUI |
  *                   |      |      |      |/       /         \      \ |      |[Caps]|      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -25,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_COLEMAK] = LAYOUT( \
    KC_ESC, KC_1, KC_2,    KC_3,    KC_4,   KC_5,                                    KC_6,      KC_7,            KC_8,    KC_9,    KC_0,    KC_EQL,  \
    KC_TAB, KC_Q, KC_W,    KC_F,    KC_P,   KC_B,                                    KC_J,      KC_L,            KC_U,    KC_Y,    KC_SCLN, KC_MINS, \
-  KC_BSPC, KC_A, KC_R,    KC_S,    KC_T,   KC_G,                                    KC_M,      KC_N,            KC_E,    KC_I,    KC_O,    KC_GRV,  \
-  KC_LBRC, KC_Z, KC_X,    KC_C,    KC_D,   KC_V,        KC_COMM,  KC_DOT,           KC_K,      KC_H,            KC_QUOT, KC_BSLS, KC_SLSH, KC_RBRC, \
-                       KC_LCTL, KC_LALT, KC_MEH, LSFT_T(KC_SPC),  LT(_UTIL,KC_ENT), MO(_PAGE), RALT_T(KC_CAPS), KC_LGUI \
+  KC_LBRC, KC_A, KC_R,    KC_S,    KC_T,   KC_G,                                    KC_M,      KC_N,            KC_E,    KC_I,    KC_O,    KC_GRV,  \
+  KC_LCTL, KC_Z, KC_X,    KC_C,    KC_D,   KC_V,        KC_COMM,  KC_DOT,           KC_K,      KC_H,            KC_QUOT, KC_BSLS, KC_SLSH, KC_RBRC, \
+                       KC_BSPC, KC_LALT, KC_MEH, LSFT_T(KC_SPC),  LT(_UTIL,KC_ENT), MO(_PAGE), RALT_T(KC_CAPS), KC_LGUI \
 ),
 
 /* QWERTY
