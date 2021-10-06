1352ac27d0b3ad66a17a2d6876ee6636a6fea639
farefray <30721331+farefray@users.noreply.github.com>
2021-10-06 12:11:49 +0300

    bugfixes to keymappings

--- keymap.c
+++ keymap.c
@@ -27,10 +27,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_COLEMAK] = LAYOUT( \
-   KC_GESC, KC_1, KC_2,    KC_3,    KC_4,   KC_5,                                    KC_6,      KC_7,            KC_8,    KC_9,    KC_0,    KC_EQL,  \
+   LALT_T(KC_ESC), KC_1, KC_2,    KC_3,    KC_4,   KC_5,                           KC_6,      KC_7,            KC_8,    KC_9,    KC_0,    KC_EQL,  \
    KC_TAB, KC_Q, KC_W,    KC_F,    KC_P,   KC_B,                                    KC_J,      KC_L,            KC_U,    KC_Y,    KC_GRV, KC_MINS, \
   KC_LBRC, KC_A, KC_R,    KC_S,    KC_T,   KC_G,                                    KC_M,      KC_N,            KC_E,    KC_I,    KC_O,    KC_SCLN,  \
-  KC_LCPO, KC_Z, KC_X,    KC_C,    KC_D,   KC_V,        KC_COMM,  KC_DOT,           KC_K,      KC_H,            KC_QUOT, KC_BSLS, KC_SLSH, KC_LCTRL, \
+  KC_LCTRL, KC_Z, KC_X,    KC_C,    KC_D,   KC_V,        KC_COMM,  KC_DOT,           KC_K,      KC_H,            KC_QUOT, KC_BSLS, KC_SLSH, KC_RBRC, \
           COPY_MACRO, LT(_UTIL, KC_KP_EQUAL), KC_BSPC, LSFT_T(KC_SPC),  LT(_UTIL,KC_ENT), MO(_PAGE), RALT_T(KC_CAPS), KC_LGUI \
 ),
 
