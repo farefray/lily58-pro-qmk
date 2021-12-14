f9ee567b2042582fbe9f7847e9727dc1a5e31cf6
farefray <30721331+farefray@users.noreply.github.com>
2021-12-14 18:16:01 +0200

    removed gui and switched to left part as master

--- config.h
+++ config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define MASTER_RIGHT
+#define MASTER_LEFT
 
 #define TAPPING_TERM 175
 
--- macros.c
+++ macros.c
@@ -1,4 +1,5 @@
 bool is_copied = false;
+bool is_gui_pressed = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
@@ -40,6 +41,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
             }
         }
         return false;
+    }
+
+    // hack for windows layer switching together with language
+    if (keycode == KC_LGUI) {
+        if (record->event.pressed)
+            is_gui_pressed = true;
+        else
+            is_gui_pressed = false;
+    }
+    
+    if (keycode == KC_SPC) {
+        if (record->event.pressed) {
+            if (is_gui_pressed == true ) {
+                layer_invert(_QWERTY);
+            }
+        }
     }
 
     return true;
